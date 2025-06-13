7d5290a950cceae2117f3b40afd83febef29a32f
hashashin <hubermanandrew5@gmail.com>
2025-06-13 16:19:59 +0800

    Auto-sync: 2025-06-13 16:19:59

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import cadquery as cq
 from cadquery import importers, exporters
 
 workplane = importers.importStep("/Users/hashashin/Documents/useful_information/20250528-探野T4-3D打印手板加工/零件3D/13镜头.stp")
+exporters.export(workplane, "test.stl")
 
-asm = cq.Assembly()
-asm.add(workplane.val())
-
-exporters.export(
-    asm,
-    "output_model.glb",
-    tolerance=0.05,
-    angularTolerance=0.3
-)
 
 print("✓ exported to output_model.glb")
