c06339df80e39061fc7db1ee5332c342f4e6b042
hashashin <hubermanandrew5@gmail.com>
2025-06-16 14:12:51 +0800

    Auto-sync: 2025-06-16 14:12:51

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ from cadquery import importers, exporters
 workplane = importers.importStep("/Users/hashashin/Documents/useful_information/20250528-探野T4-3D打印手板加工/零件3D/13镜头.stp")
 exporters.export(workplane, "test.stl")
 
-
-print("✓ exported to output_model.glb")
