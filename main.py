aa43d3a76510c73efdaed0a82845265ba6934ed8
hashashin <hubermanandrew5@gmail.com>
2025-06-16 14:13:54 +0800

    Auto-sync: 2025-06-16 14:13:54

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cadquery as cq
 from cadquery import importers, exporters
 
-workplane = importers.importStep("/Users/hashashin/Documents/useful_information/20250528-探野T4-3D打印手板加工/零件3D/13镜头.stp")
+workplane = importers.importStep("/Users/hashashin/Documents/useful_information/20250528-探野T4-3D打印手板加工/零件3D/4按键1.stp")
 exporters.export(workplane, "test.stl")
 
